e030c3a727ea38b7dfe48b62c57ab5163a1f95f5
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-05-24 17:24:20 +0300

    Done Full Project

--- Calculator_App/Calculator_App/SimpleCalculator.Designer.cs
+++ Calculator_App/Calculator_App/SimpleCalculator.Designer.cs
@@ -51,6 +51,8 @@ namespace Calculator_App
             this.button16 = new System.Windows.Forms.Button();
             this.button17 = new System.Windows.Forms.Button();
             this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,11 +60,13 @@ namespace Calculator_App
             // 
             this.button1.AutoSize = true;
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(4)))), ((int)(((byte)(6)))));
+            this.button1.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Bold);
             this.button1.Location = new System.Drawing.Point(321, 351);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 75);
             this.button1.TabIndex = 3;
             this.button1.Text = "+";
+            this.button1.UseCompatibleTextRendering = true;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
@@ -70,11 +74,13 @@ namespace Calculator_App
             // 
             this.button2.AutoSize = true;
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(4)))), ((int)(((byte)(6)))));
+            this.button2.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Bold);
             this.button2.Location = new System.Drawing.Point(321, 432);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 75);
             this.button2.TabIndex = 1;
             this.button2.Text = "X";
+            this.button2.UseCompatibleTextRendering = true;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -82,11 +88,14 @@ namespace Calculator_App
             // 
             this.button3.AutoSize = true;
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(4)))), ((int)(((byte)(6)))));
+            this.button3.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Bold);
             this.button3.Location = new System.Drawing.Point(240, 351);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 75);
             this.button3.TabIndex = 4;
             this.button3.Text = "-";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button3.UseCompatibleTextRendering = true;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
@@ -94,11 +103,13 @@ namespace Calculator_App
             // 
             this.button4.AutoSize = true;
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(4)))), ((int)(((byte)(6)))));
+            this.button4.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Bold);
             this.button4.Location = new System.Drawing.Point(240, 432);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 75);
             this.button4.TabIndex = 2;
             this.button4.Text = "/";
+            this.button4.UseCompatibleTextRendering = true;
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -106,11 +117,13 @@ namespace Calculator_App
             // 
             this.button5.AutoSize = true;
             this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button5.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button5.Location = new System.Drawing.Point(12, 436);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(70, 70);
             this.button5.TabIndex = 3;
             this.button5.Text = "4";
+            this.button5.UseCompatibleTextRendering = true;
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
@@ -118,6 +131,7 @@ namespace Calculator_App
             // 
             this.button6.AutoSize = true;
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button6.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button6.Location = new System.Drawing.Point(88, 436);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(70, 70);
@@ -130,11 +144,13 @@ namespace Calculator_App
             // 
             this.button7.AutoSize = true;
             this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button7.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button7.Location = new System.Drawing.Point(12, 512);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(70, 70);
             this.button7.TabIndex = 5;
             this.button7.Text = "7";
+            this.button7.UseCompatibleTextRendering = true;
             this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
@@ -142,11 +158,13 @@ namespace Calculator_App
             // 
             this.button8.AutoSize = true;
             this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button8.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button8.Location = new System.Drawing.Point(88, 512);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(70, 70);
             this.button8.TabIndex = 6;
             this.button8.Text = "8";
+            this.button8.UseCompatibleTextRendering = true;
             this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
@@ -154,11 +172,13 @@ namespace Calculator_App
             // 
             this.button9.AutoSize = true;
             this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button9.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button9.Location = new System.Drawing.Point(12, 360);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(70, 70);
             this.button9.TabIndex = 7;
             this.button9.Text = "1";
+            this.button9.UseCompatibleTextRendering = true;
             this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
@@ -166,11 +186,13 @@ namespace Calculator_App
             // 
             this.button10.AutoSize = true;
             this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button10.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button10.Location = new System.Drawing.Point(88, 360);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(70, 70);
             this.button10.TabIndex = 8;
             this.button10.Text = "2";
+            this.button10.UseCompatibleTextRendering = true;
             this.button10.UseVisualStyleBackColor = false;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
@@ -178,11 +200,13 @@ namespace Calculator_App
             // 
             this.button11.AutoSize = true;
             this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button11.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button11.Location = new System.Drawing.Point(164, 360);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(70, 70);
             this.button11.TabIndex = 9;
             this.button11.Text = "3";
+            this.button11.UseCompatibleTextRendering = true;
             this.button11.UseVisualStyleBackColor = false;
             this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
@@ -190,11 +214,13 @@ namespace Calculator_App
             // 
             this.button12.AutoSize = true;
             this.button12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button12.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button12.Location = new System.Drawing.Point(164, 436);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(70, 70);
             this.button12.TabIndex = 10;
             this.button12.Text = "6";
+            this.button12.UseCompatibleTextRendering = true;
             this.button12.UseVisualStyleBackColor = false;
             this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
@@ -202,11 +228,13 @@ namespace Calculator_App
             // 
             this.button13.AutoSize = true;
             this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button13.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold);
             this.button13.Location = new System.Drawing.Point(164, 512);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(70, 70);
             this.button13.TabIndex = 5;
             this.button13.Text = "9";
+            this.button13.UseCompatibleTextRendering = true;
             this.button13.UseVisualStyleBackColor = false;
             this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
@@ -216,7 +244,8 @@ namespace Calculator_App
             this.textBox1.Location = new System.Drawing.Point(12, 37);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(384, 284);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(382, 284);
             this.textBox1.TabIndex = 11;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -230,7 +259,7 @@ namespace Calculator_App
             this.panel1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(405, 33);
+            this.panel1.Size = new System.Drawing.Size(406, 39);
             this.panel1.TabIndex = 12;
             // 
             // label1
@@ -248,11 +277,11 @@ namespace Calculator_App
             this.button14.AutoSize = true;
             this.button14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
             this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold);
+            this.button14.Font = new System.Drawing.Font("Calibri", 10F);
             this.button14.ForeColor = System.Drawing.Color.Red;
             this.button14.Location = new System.Drawing.Point(361, 3);
             this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(42, 27);
+            this.button14.Size = new System.Drawing.Size(42, 33);
             this.button14.TabIndex = 0;
             this.button14.Text = "X";
             this.button14.UseVisualStyleBackColor = false;
@@ -267,6 +296,7 @@ namespace Calculator_App
             this.button15.Size = new System.Drawing.Size(70, 70);
             this.button15.TabIndex = 13;
             this.button15.Text = "0";
+            this.button15.UseCompatibleTextRendering = true;
             this.button15.UseVisualStyleBackColor = false;
             this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
@@ -274,11 +304,13 @@ namespace Calculator_App
             // 
             this.button16.AutoSize = true;
             this.button16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button16.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Bold);
             this.button16.Location = new System.Drawing.Point(324, 512);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(70, 70);
             this.button16.TabIndex = 14;
             this.button16.Text = "=";
+            this.button16.UseCompatibleTextRendering = true;
             this.button16.UseVisualStyleBackColor = false;
             this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
@@ -306,12 +338,38 @@ namespace Calculator_App
             this.button18.UseVisualStyleBackColor = false;
             this.button18.Click += new System.EventHandler(this.button18_Click);
             // 
+            // button19
+            // 
+            this.button19.AutoSize = true;
+            this.button19.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.button19.Location = new System.Drawing.Point(174, 323);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(60, 26);
+            this.button19.TabIndex = 17;
+            this.button19.Text = "Ans";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.AutoSize = true;
+            this.button20.BackColor = System.Drawing.Color.Green;
+            this.button20.Location = new System.Drawing.Point(12, 323);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(40, 31);
+            this.button20.TabIndex = 18;
+            this.button20.Text = "On";
+            this.button20.UseVisualStyleBackColor = false;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
             // SimpleCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(68)))), ((int)(((byte)(25)))));
-            this.ClientSize = new System.Drawing.Size(405, 626);
+            this.ClientSize = new System.Drawing.Size(406, 601);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
             this.Controls.Add(this.button18);
             this.Controls.Add(this.button17);
             this.Controls.Add(this.button16);
@@ -365,6 +423,8 @@ namespace Calculator_App
         private Button button16;
         private Button button17;
         private Button button18;
+        private Button button19;
+        private Button button20;
     }
 }
 
--- Calculator_App/Calculator_App/SimpleCalculator.cs
+++ Calculator_App/Calculator_App/SimpleCalculator.cs
@@ -10,6 +10,7 @@ namespace Calculator_App
     {
         // Fields
         private string x = ""; // Non-static instance field
+        private string ans = "";
 
         // Draggable window setup
         [DllImport("user32.dll")]
@@ -35,6 +36,70 @@ namespace Calculator_App
             ReleaseCapture();
             SendMessage(this.Handle, 0xA1, 0x2, 0);
         }
+
+        #region On/Off Button
+        bool on = true;
+        
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if(on == false)
+            {
+                on = true;
+                button20.BackColor = Color.Green;
+            }
+            else
+            {
+                on = false;
+                button20.BackColor = Color.Red;
+
+            }
+
+            Checker(on);
+        }
+        private void Checker(bool on)
+        {
+            if (on == true)
+            {
+                textBox1.Enabled = true;
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+                button10.Enabled = true;
+                button11.Enabled = true;
+                button12.Enabled = true;
+                button13.Enabled = true;
+                button15.Enabled = true;
+                button16.Enabled = true;
+            }
+            else
+            {
+                textBox1.Text = "";
+                x = "";
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+                button10.Enabled = false;
+                button11.Enabled = false;
+                button12.Enabled = false;
+                button13.Enabled = false;
+                button15.Enabled = false;
+                button16.Enabled = false;
+            }
+        }
+        #endregion
 
         #region Number Buttons
         private void button5_Click(object sender, EventArgs e)  // 4
@@ -122,6 +187,40 @@ namespace Calculator_App
             x += "/";
             textBox1.Text = x;
         }
+        private void button17_Click(object sender, EventArgs e)
+        {
+            x = "";
+            textBox1.Text = x;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            x += "+";
+            textBox1.Text = x;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string y = x;
+            x = "";
+            for (int i=0; i<y.Length - 1; i++)
+            {
+                x += y[i];
+            }
+            textBox1.Text = x;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            x+= "-";
+            textBox1.Text = x;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            x = ans;
+            textBox1.Text = x;
+        }
         #endregion
 
         #region Special Functions
@@ -165,7 +264,8 @@ namespace Calculator_App
                         }
 
 
-                        textBox1.Text += " = " + x;
+                        textBox1.Text += " = \r\n" + x;
+                        ans = x;    // to save the last answer in variable ans
                         break;
                     }
                 }
@@ -189,36 +289,14 @@ namespace Calculator_App
         #region Unused Event Handlers (Keep for designer compatibility)
         private void Form1_Load(object sender, EventArgs e) { }
         private void label1_Click(object sender, EventArgs e) { }
-        private void textBox1_TextChanged(object sender, EventArgs e) { }
-        #endregion
-
-        private void button17_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            x = "";
-            textBox1.Text = x;
+            textBox1.ScrollBars = ScrollBars.Horizontal;
+            textBox1.WordWrap = false; // Disable word wrap for better display of long expressions
+            textBox1.Font = new Font("DS-Digital", 40); // Set font and size
         }
+            #endregion
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            x += "+";
-            textBox1.Text = x;
-        }
 
-        private void button18_Click(object sender, EventArgs e)
-        {
-            string y = x;
-            x = "";
-            for (int i=0; i<y.Length - 1; i++)
-            {
-                x += y[i];
-            }
-            textBox1.Text = x;
         }
-
-        private void button3_Click_1(object sender, EventArgs e)
-        {
-            x+= "-";
-            textBox1.Text = x;
-        }
-    }
 }
